6849b7f5bc1a0fafb01b1417e2f432716d9d5463
Marco Jimenez <jimenez.marco.dev@outlook.com>
2024-02-27 16:40:37 -0600

    unit testing coverage

---
+++ src/test/java/com/challenge/algorithms/AlgorithmsApplicationTest.java
@@ -0,0 +1,14 @@
+package com.challenge.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+@ExtendWith(MockitoExtension.class)
+class AlgorithmsApplicationTest {
+
+	@Test
+	void contextLoads() {
+		AlgorithmsApplication.main(new String[] {});
+	}
+
+}
